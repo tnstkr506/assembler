09aba057547bcccedf399754854fe105857cfa7f
Jeon JunHo <king9278@naver.com>
2018-11-12 23:25:40 +0900

    add comment

--- check.c
+++ check.c
@@ -2,34 +2,34 @@
 char* reg[8] = { "%eax","%ecx","%edx","%ebx", "%esp","%ebp","%esi","%edi" };
 char* memReg[8] = { "(%eax)","(%ecx)","(%edx)","(%ebx)", "(%esp)","(%ebp)","(%esi)","(%edi)" };
 
-int RegEqual(char *arg) {
+int RegEqual(char *arg) {//check reg
 	int i = 0;
 	for (i = 0; i < 8; i++) {
-		if (strcmp(arg, reg[i]) == 0)
-			return REG;
+		if (strcmp(arg, reg[i]) == 0)//reg equal regarray
+			return REG;//arg type reg
 	}
 	return 0;
 }
-int MemEqual(char *arg) {
+int MemEqual(char *arg) {//check memory
 	int i = 0;
 	for (i = 0; i < 8; i++) {
-		if (strstr(arg, memReg[i]) != NULL)
-			return DIS;
+		if (strstr(arg, memReg[i]) != NULL)// (reg) exist in arg
+			return DIS;//arg type dis mem
 	}
-	if (arg[0]!='$' && strstr(arg, "0x") != NULL)
-		return MEM;
+	if (arg[0]!='$' && strstr(arg, "0x") != NULL)//not Imd & memory
+		return MEM;//arg type mem (dis X)
 	return 0;
 }
 int is_valid(char *op, char *arg1, char * arg2)
 {
 	//printf("if valid, return 1\n");
 	//printf("otherwise, return 0\n");
-	if (strcmp(op, "mov")!=0)
-		return 0;
-	int MResult1 = MemEqual(arg1);
-	int MResult2 = MemEqual(arg2);
-	int RResult1 = RegEqual(arg1);
-	int RResult2 = RegEqual(arg2);
+	if (strcmp(op, "mov")!=0)//op is not move
+		return 0;//error
+	int MResult1 = MemEqual(arg1);//is arg1 mem?
+	int MResult2 = MemEqual(arg2);//is arg2 mem?
+	int RResult1 = RegEqual(arg1);//is arg1 reg?
+	int RResult2 = RegEqual(arg2);//is arg2 reg?
 	if (MResult1 == DIS) {//arg1 : mem(disp) 
 	//mem to reg (addressing w.o/w disp.)
 		if (RegEqual(arg2)) {//arg2 : reg
--- trans.c
+++ trans.c
@@ -1,13 +1,13 @@
 #include "myas.h"
 
 extern int is_valid(char *op, char* arg1, char* arg2);
-extern char* reg[8];
+extern char* reg[8];// extern reg array
 //"%eax","%ecx","%edx","%ebx", "%esp","%ebp","%esi","%edi"
-int RegType(char *arg) {
+int RegType(char *arg) {//type of reg
 	int i = 0;
 	for (i = 0; i < 8; i++) {
-		if (strcmp(arg, reg[i]) == 0)
-			return i;
+		if (strcmp(arg, reg[i]) == 0)//check arg equal regarray
+			return i;//type of reg 
 	}
 }
 
@@ -16,20 +16,20 @@ int instr_trans(char *op, char *args, char* mcode)
 	// check syntax 
 	char token[256];
 	int i = 0;
-	char hex = 8;
-	strcpy(token, args);
-	char *arg1 = strtok(token, ",");
-	char *arg2 = strtok(NULL, ",");
-	int RETVAL;
+	char hex = 8;//Imd to Reg opcode='b'+hex;
+	strcpy(token, args);//token copy args
+	char *arg1 = strtok(token, ",");//arg1
+	char *arg2 = strtok(NULL, ",");//arg2
+	int RETVAL;//return value
 	char opcode[10] = "00";
-	printf("%s %s ", arg1, arg2);
+	printf("%s %s ", arg1, arg2);//console print
 
-	if (!(RETVAL = is_valid(op, arg1, arg2))) {
+	if (!(RETVAL = is_valid(op, arg1, arg2))) {//is_valid return 0 ->error
 		printf("Error: %s %s is not valid\n", op, args);
-		strcpy(mcode, "error");
+		strcpy(mcode, "error");//mcode fprint mov.out error
 		return 0;
 	}
-	//{RTR = 1, MTR, MTR_WO, MTR_W, MTR_EAX, RTM, ITR};
+	//enum {RTR = 1, MTRD, MTR_WO, MTR_W, MTR_EAX, RTM, ITR};
 	switch (RETVAL) {
 	case RTR://reg to reg
 		strcpy(opcode, "89");
@@ -45,19 +45,19 @@ int instr_trans(char *op, char *args, char* mcode)
 		break;
 	case ITR://imd to reg
 		//"%eax","%ecx","%edx","%ebx", "%esp","%ebp","%esi","%edi"
-		hex += RegType(arg2);
-		if (hex >= 10)
-			hex = 'a' + (hex - 10);
-		else
-			hex = hex + '0';
-		opcode[0] = 'b'; opcode[1] = hex; opcode[2] = 0;
+		hex += RegType(arg2);//hex= 8+regtype
+		if (hex >= 10)//decimal -> hex
+			hex = 'a' + (hex - 10);//int-> char
+		else//hex<10 eax OR ecx
+			hex = hex + '0';//int -> char
+		opcode[0] = 'b'; opcode[1] = hex; opcode[2] = 0;//\0
 		break;
 	default:
 		strcpy(opcode, "error");
 	}
 	strcpy(mcode, opcode);
 
-	printf("%s\n", mcode);
+	printf("%s\n", mcode);//console print
 	return 1;
 }
 
